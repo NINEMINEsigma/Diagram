37a80586793add546817e1085f45659845e824e1
ninemine <106434473+NINEMINEsigma@users.noreply.github.com>
2024-07-20 22:20:59 +0800

    +LineScript
    TODO位于解析单词处

--- Assets/Diagram/Core/LineScript/LineScript.Core.cs
+++ Assets/Diagram/Core/LineScript/LineScript.Core.cs
@@ -1,13 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.IO;
+using System.IO; 
 using UnityEditor.AssetImporters;
 using UnityEngine;
 
 namespace Diagram
 {
-    public class LineScriptCore 
+
+    [System.Serializable]
+    public class LineScriptException : DiagramException
     {
+        public LineScriptException() { }
+        public LineScriptException(string message) : base(message) { }
+        public LineScriptException(string message, System.Exception inner) : base(message, inner) { }
+        protected LineScriptException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+
+    [System.Serializable]
+    public class ParseException : LineScriptException
+    {
+        public ParseException(string message) : base("Bad Parse: " + message) { }
+    }
+
+    public class LineScript
+    {
+        #region env
+        public Dictionary<string, object> CreatedInstances = new();
+        #endregion
+
         public void Run(LineScriptAssets ls)
         {
             Run(ls.text);
@@ -18,7 +40,73 @@ namespace Diagram
         }
         private void CoreRun(string[] ls)
         {
-
+            foreach (var line in ls)
+            {
+                List<string> words = new();
+                string current = "";
+                bool isnot_in_literal_value = false;
+                bool isnot_skip = false;
+                foreach (var ch in line)
+                {
+                    switch (ch)
+                    {
+                        case ' ' when isnot_in_literal_value&& isnot_skip:
+                            {
+                                if (current.Length != 0)
+                                    words.Add(current);
+                                current = "";
+                            }
+                            break;
+                        case '\"' when isnot_in_literal_value&& isnot_skip:
+                            {
+                                isnot_in_literal_value = true;
+                                current += '\"';
+                            }
+                            break;
+                        case '\"' when isnot_in_literal_value==false && isnot_skip:
+                            {
+                                isnot_in_literal_value = false;
+                                current += '\"';
+                            }
+                            break;
+                        case '\\' when isnot_in_literal_value==false && isnot_skip:
+                            {
+                                isnot_skip = true;
+                                current += '\\';
+                            }
+                            break;
+                        default:
+                            current += ch;
+                            isnot_skip = false;
+                            break;
+                    }
+                }
+                if (current.Length > 0) words.Add(current);
+                CoreLineParse(words.ToArray());
+            }
+        }
+        private void CoreLineParse(string[] words)
+        {
+            if (words.Length == 0) return;
+            List<LineWord> LineWords = new();
+            LineWord Controller = AllowFirstWord.StaticInstance;
+            foreach (var word in words)
+            {
+                LineWord lineWord = LineWord.Read(word);
+                if (Controller.DetectNext(lineWord))
+                {
+                    //TODO
+                }
+                else throw new ParseException($"\"{word}\" is not allow");
+                Controller = lineWord;
+            }
+        }
+        private class AllowFirstWord:LineWord
+        {
+            public override bool AllowLinkKeyWord => true;
+            public override bool AllowLinkLiteralValue => true;
+            public override bool AllowLinkSymbolWord => true;
+            public readonly static AllowFirstWord StaticInstance = new AllowFirstWord();
         }
     }
 
--- Assets/Diagram/Core/LineScript/LineScript.Lang.cs
+++ Assets/Diagram/Core/LineScript/LineScript.Lang.cs
@@ -4,24 +4,101 @@ using UnityEngine;
 
 namespace Diagram
 {
+    public static class LineLanguageUnit
+    {
+        public static void InitLineLanguage()
+        {
+            LineWord.WordPairs = new()
+            {
+                { "using",new SystemKeyWord.using_Key()},
+                { "import",new SystemKeyWord.import_Key()},
+                { "if",new SystemKeyWord.if_Key()},
+                { "else",new SystemKeyWord.else_Key()},
+                { "while",new SystemKeyWord.while_Key()},
+                { "for",new SystemKeyWord.for_Key()},
+                { "break",new SystemKeyWord.break_Key()},
+                { "continue",new SystemKeyWord.continue_Key()},
+                { "define",new SystemKeyWord.define_Key()}
+            };
+        }
+    }
+
     public abstract class LineWord
     {
         public static Dictionary<string, LineWord> WordPairs = new();
-        public bool IsLiteralValue { get; protected set; } = false;
-        public bool IsKeyWord { get; protected set; } = false;
-        public bool IsSymbolWord { get; protected set; } = false;
-        public bool AllowLinkLiteralValue { get; protected set; } = false;
-        public bool AllowLinkKeyWord { get; protected set; } = false;
-        public bool AllowLinkSymbolWord { get; protected set; } = false;
+        public virtual bool IsLiteralValue { get => false; }
+        public virtual bool IsKeyWord { get => false; }
+        public virtual bool IsSymbolWord { get => false; }
+        public virtual bool AllowLinkLiteralValue { get => false; }
+        public virtual bool AllowLinkKeyWord { get => false; }
+        public virtual bool AllowLinkSymbolWord { get => false; }
+        public static LineWord Read(string source)
+        {
+            if (source[0]=='\"')
+            {
+                if (source[^1] == '\"') return new LiteralValueWord(source[1..^1]);
+                else throw new ParseException(source);
+            }
+            else if(WordPairs.TryGetValue(source,out var word))
+            {
+                return word;
+            }
+            else
+            {
+                return SymbolWord.GetSymbolWord(source);
+            }
+        }
+        public bool DetectNext(LineWord next)
+        {
+            if (next.IsSymbolWord == true && this.AllowLinkSymbolWord==false) return false;
+            if (next.IsKeyWord == true && this.AllowLinkKeyWord==false) return false;
+            if (next.IsLiteralValue == true && this.AllowLinkLiteralValue == false) return false;
+            return true;
+        }
     }
     public abstract class SystemKeyWord : LineWord
     {
-        public class Using:SystemKeyWord
-        {
-            public Using()
-            {
+        public override bool IsKeyWord => true;
+        public class using_Key : SystemKeyWord { public override bool AllowLinkLiteralValue => true; }
+        public class import_Key : SystemKeyWord { public override bool AllowLinkLiteralValue => true; }
+        public class if_Key : SystemKeyWord { public override bool AllowLinkLiteralValue => true; }
+        public class else_Key : SystemKeyWord { public override bool AllowLinkLiteralValue => true; public override bool AllowLinkKeyWord => true; }
+        public class while_Key : SystemKeyWord { public override bool AllowLinkLiteralValue => true; }
+        public class for_Key : SystemKeyWord { public override bool AllowLinkLiteralValue => true; }
+        public class break_Key : SystemKeyWord { }
+        public class continue_Key : SystemKeyWord { }
+        public class define_Key : SystemKeyWord { public override bool AllowLinkLiteralValue => true; }
+    }
 
-            }
+    public class LiteralValueWord : LineWord
+    {
+        public string source;
+        public override bool IsLiteralValue => true;
+        public override bool AllowLinkKeyWord => true;
+        public LiteralValueWord(string source)
+        {
+            this.source = source;
+        }
+    }
+
+    public class SymbolWord:LineWord
+    {
+        public string source;
+        public override bool IsSymbolWord => true;
+        public override bool AllowLinkKeyWord => true;
+        public SymbolWord(string source)
+        {
+            this.source = source;
+        }
+
+        public class OperatorKeyWord:SymbolWord
+        {
+            public OperatorKeyWord(string source) : base(source) { }
+        }
+
+        public static SymbolWord GetSymbolWord(string source)
+        {
+            return new SymbolWord(source);
         }
     }
 }
